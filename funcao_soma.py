118a438b7995521145acbb737d2ae0c61d297e2d
Fabio Marques <fabinho_marquez@hotmail.com.br>
2025-10-29 23:36:46 -0300

    feat: novo exercício

---
+++ funcao_soma.py
@@ -0,0 +1,9 @@
+def calculo(num1, num2):
+    soma = num1 + num2
+    resultado = print(f'A soma de {num1} + {num2} é igual a: {soma}')
+    return resultado
+
+num1 = 10
+num2 = 40
+
+calculo(num1, num2)
